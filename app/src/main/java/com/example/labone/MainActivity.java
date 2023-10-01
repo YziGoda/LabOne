40c8119e2f1b1f06adbd6cf5cbc23bad373e712d
YziGoda <yzigoda@gmail.com>
2023-10-01 12:52:59 +0300

    Added a comment that

--- app/src/main/java/com/example/labone/MainActivity.java
+++ app/src/main/java/com/example/labone/MainActivity.java
@@ -21,4 +21,6 @@ public class MainActivity extends AppCompatActivity {
     public void onBtnChangeTextClick(View view) {
         this.tvMain.setText("Labas pasauli");
     }
+
+    //Some extra task that we will revert
 }
