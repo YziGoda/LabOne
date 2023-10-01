64571af31b4871356ecabc6a4b76e9f874c95fdd
YziGoda <yzigoda@gmail.com>
2023-10-01 12:49:26 +0300

    Changes done according to the lesson. Added change text functionality

--- app/src/main/java/com/example/labone/MainActivity.java
+++ app/src/main/java/com/example/labone/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.labone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnChangeTextClick(View view) {
+        this.tvMain.setText("Labas pasauli");
     }
 }
